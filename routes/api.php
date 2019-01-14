ad5ea662b5efd76c6f4a3210078cba7266187435
hoaittt <to.thi.thu.hoai@framgia.com>
2019-01-14 14:25:05 +0700

    api crud task

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,13 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('tasks', 'TaskController@index');
+Route::get('task/{id}', 'TaskController@show');
+Route::post('task', 'TaskController@store');
+Route::put('task/{id}', 'TaskController@update');
+Route::delete('task/{id}', 'TaskController@delete');
+
 Route::group(['namespace' => 'Api'], function () {
     Route::post('user/register', 'UsersController@register');
 
